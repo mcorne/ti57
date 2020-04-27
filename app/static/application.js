2c9f8bc079b91c1fbd229b6b0a7b0d9398650bd0
Michel Corne <mcorne@yahoo.com>
2020-04-27 19:21:46 +0200

    fixed textarea height autofitting on window resize

--- app/static/application.js
+++ app/static/application.js
@@ -1,10 +1,19 @@
 
 // https://stackoverflow.com/questions/23451611/is-there-anyway-to-have-a-textarea-autofit-height-based-on-the-content-at-page
-function autofit_textarea_height(name) {
-    var textarea = document.getElementById(name);
+function autofit_textarea_height() {
+    var addon;
+    var textarea = document.getElementById("ti_instructions");
 
     if (textarea.clientHeight < textarea.scrollHeight) {
-        textarea.style.height = textarea.scrollHeight + 5 + "px";
+        if (screen.width < 601) { // w3-hide-small
+            addon = 10
+        } else if (screen.width < 992) { // w3-hide-medium
+            addon = 7
+        } else { // w3-hide-large
+            addon = 5
+        }
+
+        textarea.style.height = textarea.scrollHeight + addon + "px";
     }
 }
 
