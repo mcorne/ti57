ad83de0bf873533c28c111f188023f4a4a74e9be
Michel Corne <mcorne@yahoo.com>
2020-05-12 17:26:29 +0200

    added docstrings in parser file

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,8 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    # Create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(SECRET_KEY="dev")
-
-    if test_config is None:
-        # Load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # Load the test config if passed in
-        app.config.from_mapping(test_config)
-
+    app = Flask(__name__)
+    app.secret_key = "123456"  # no need for secrecy here!
     from .views import program
 
     app.register_blueprint(program.bp)
--- app/models/parser.py
+++ app/models/parser.py
@@ -2,18 +2,23 @@ import re
 
 
 class Parser:
+    """TI instructions Parser."""
+
     DECIMAL = r"([0-9]*\.[0-9]+)|([0-9]+\.[0-9]*)"
     INTEGER = r"[0-9]+"
     # https://docs.python.org/3/howto/regex.html#regex-howto
     METACHARACTERS = r"[.\^$*+?{}[\]|()]"
     MISMATCH = r"\S+"
 
-    def __init__(self, ti_instructions, description, ti_instruction_set):
+    def __init__(self, ti_instructions, description, ti_instruction_set, init=True):
         self.description = description
-        self.ti_instructions = ti_instructions  # expecting \n line ending
+        self.ti_instructions = ti_instructions  # IMPORTANT! expecting \n line endings
         self.ti_instruction_set = ti_instruction_set
+        if init:
+            self.init()
 
     def convert_key_to_pattern(self, key):
+        """Convert a key to a pattern, ex. STO NUMBER -> STO +[0-9]."""
         # Escape metacharacters
         # Cannot use re.escape() that escapes spaces etc. depending on Python version!
         pattern = re.sub(Parser.METACHARACTERS, r"\\\g<0>", key)
@@ -29,12 +34,14 @@ class Parser:
         return pattern
 
     def fix_number_in_py_lines(self, number, py_lines):
+        """Replace NUMBER in Python lines with the actual number, ex. mem[NUMBER] -> mem[7]"""
         if type(py_lines) is not list:
             py_lines = [py_lines]
         py_lines = [py_line.replace("NUMBER", number) for py_line in py_lines]
         return py_lines
 
     def get_original_key_and_number(self, groups):
+        """Return the original key and number from the matching group, ex. _KEY_STO_NUMBER, 7 -> STO NUMBER, 7."""
         group_name, number = next(
             (
                 [group_name, number]
@@ -49,20 +56,33 @@ class Parser:
             original_key = None
         return [original_key, number]
 
-    def move_inline_comments_up(self):
-        self.ti_instructions = re.sub(
-            r"^(.*?[^ \n] *)(#.*?)$", r"\g<2>\n\g<1>", self.ti_instructions, 0, re.M
-        )
-
-    # See https://docs.python.org/3.8/library/re.html#writing-a-tokenizer
-    def next_instruction(self):
+    def init(self):
+        """Convert keys to lower case, set the instruction patterns, validate the program instructions."""
         self.set_lower_case_keys()
         self.set_instruction_patterns()
         self.validate_instructions()
         self.move_inline_comments_up()
-        return self.tokenizer()
+
+    def move_inline_comments_up(self):
+        """Move instructions comments above the corresponding instruction."""
+        self.ti_instructions = re.sub(
+            r"^(.*?[^ \n] *)(#.*?)$", r"\g<2>\n\g<1>", self.ti_instructions, 0, re.M
+        )
+
+    def next_instruction(self):
+        """Yield the next instruction/key, this is the tokenizer (the entry point).
+
+            Source: # See https://docs.python.org/3.8/library/re.html#writing-a-tokenizer
+        """
+        line = 1
+        start = 0
+        for match in re.finditer(self.patterns, self.ti_instructions, re.I):
+            start, line, ti_instruction = self.process_token(match, start, line)
+            if ti_instruction["action"] != "continue":
+                yield ti_instruction
 
     def process_key_type_instruction(self, ti_instruction, groups):
+        """Process an instruction that is a key."""
         original_key, number = self.get_original_key_and_number(groups)
         if original_key is not None:
             value = original_key
@@ -88,6 +108,7 @@ class Parser:
         return ti_instruction
 
     def process_token(self, match, start, line):
+        """Process a token returned by the tokenizer."""
         column = match.start() - start
         type = match.lastgroup
         value = match.group()
@@ -121,6 +142,7 @@ class Parser:
         return [start, line, ti_instruction]
 
     def set_instruction_patterns(self):
+        """Convert the instructions/keys to patterns parsable by the tokenizer."""
         patterns = [
             self.convert_key_to_pattern(key) for key in self.ti_instruction_set.keys()
         ]
@@ -136,20 +158,13 @@ class Parser:
         self.patterns = "|".join("(?P<%s>%s)" % group for group in groups)
 
     def set_lower_case_keys(self):
+        """Convert the instructions/keys to lower case."""
         self.lower_case_keys = dict(
             [(key.lower(), key) for key in self.ti_instruction_set.keys()]
         )
 
-    # See https://docs.python.org/3.8/library/re.html#writing-a-tokenizer
-    def tokenizer(self):
-        line = 1
-        start = 0
-        for match in re.finditer(self.patterns, self.ti_instructions, re.I):
-            start, line, ti_instruction = self.process_token(match, start, line)
-            if ti_instruction["action"] != "continue":
-                yield ti_instruction
-
     def validate_instructions(self):
+        """Validate the program instructions before splitting comments from instructions."""
         line = 1
         start = 0
         for match in re.finditer(
